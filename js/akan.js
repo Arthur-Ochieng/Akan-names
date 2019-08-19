e225da29e9e3447ec666a12878cc927770b6353f
Arthur-Ochieng <johnarthur016@gmail.com>
2019-08-19 14:16:24 +0300

    Update all files

--- js/akan.js
+++ js/akan.js
@@ -1,18 +1,22 @@
+//General Array Function
 function MakeArray(n) {
-    this.length = n;
-    for (var i = 1; i <=n; i++) {
-      this[i] = 0;
-    }
- }
- days = new MakeArray(7);
- days[0] = "Saturday"
- days[1] = "Sunday"
- days[2] = "Monday"
- days[3] = "Tuesday"
- days[4] = "Wednesday"
- days[5] = "Thursday"
- days[6] = "Friday"
+   this.length = n;
+   for (var i = 1; i <=n; i++) {
+     this[i] = 0;
+   }
+}
 
+//Initialize Days of Week Array
+days = new MakeArray(7);
+days[0] = "Saturday"
+days[1] = "Sunday"
+days[2] = "Monday"
+days[3] = "Tuesday"
+days[4] = "Wednesday"
+days[5] = "Thursday"
+days[6] = "Friday"
+
+//Initialize Months Array
 months = new MakeArray(12);
 months[1] = "January" 
 months[2] = "February" 
@@ -27,15 +31,17 @@ months[10] = "October"
 months[11] = "November"
 months[12] = "December"
 
+//Akan names Array
 maleakannames = new MakeArray(7);
 maleakannames[0]= "kwame"
 maleakannames[1]= "kwasi"
 maleakannames[2]= "kwadwo"
 maleakannames[3]= "kwabena"
-maleakannames[4]= "kwaki"
+maleakannames[4]= "kwaku"
 maleakannames[5]= "yaw"
 maleakannames[6]= "kwame"
 
+//Akan names Array
 femaleakannames = new MakeArray(7);
 femaleakannames[0]= "Ama"
 femaleakannames[1]= "Akosua"
@@ -45,40 +51,9 @@ femaleakannames[4]= "Akua"
 femaleakannames[5]= "Yaa"
 femaleakannames[6]= "Afua"
 
+
+//Day of Week Function
 function malecompute(form) {
-    var val1 = parseInt(form.day.value, 10)
-    if ((val1 < 0) || (val1 > 31)) {
-       alert("Day is out of range")
-    }
-    var val2 = parseInt(form.month.value, 10)
-    if ((val2 < 0) || (val2 > 12)) {
-       alert("Month is out of range")
-    }  
-    var val2x = parseInt(form.month.value, 10)
-    var val3 = parseInt(form.year.value, 10)
-    if (val3 < 1900) {
-       alert("You're quite old!")
-    }
-    if (val2 == 1) {
-       val2x = 13;
-       val3 = val3-1
-    }
-    if (val2 == 2) {
-       val2x = 14;
-       val3 = val3-1
-    }
-    var val4 = parseInt(((val2x+1)*3)/5, 10)
-    var val5 = parseInt(val3/4, 10)
-    var val6 = parseInt(val3/100, 10)
-    var val7 = parseInt(val3/400, 10)
-    var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
-    var val9 = parseInt(val8/7, 10)
-    var val0 = val8-(val9*7)
-    form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
-    form.result2.value = maleakannames[val0]
- }   
- 
- function femalecompute(form) {
    var val1 = parseInt(form.day.value, 10)
    if ((val1 < 0) || (val1 > 31)) {
       alert("Day is out of range")
@@ -90,7 +65,7 @@ function malecompute(form) {
    var val2x = parseInt(form.month.value, 10)
    var val3 = parseInt(form.year.value, 10)
    if (val3 < 1900) {
-      alert("You're that old!")
+      alert("You're quite old!")
    }
    if (val2 == 1) {
       val2x = 13;
@@ -108,7 +83,38 @@ function malecompute(form) {
    var val9 = parseInt(val8/7, 10)
    var val0 = val8-(val9*7)
    form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
-   form.result2.value = femaleakannames [val0]
- }   
- 
- 
+   form.result2.value = maleakannames[val0]
+}   
+
+function femalecompute(form) {
+  var val1 = parseInt(form.day.value, 10)
+  if ((val1 < 0) || (val1 > 31)) {
+     alert("Day is out of range")
+  }
+  var val2 = parseInt(form.month.value, 10)
+  if ((val2 < 0) || (val2 > 12)) {
+     alert("Month is out of range")
+  }  
+  var val2x = parseInt(form.month.value, 10)
+  var val3 = parseInt(form.year.value, 10)
+  if (val3 < 1900) {
+     alert("You're that old!")
+  }
+  if (val2 == 1) {
+     val2x = 13;
+     val3 = val3-1
+  }
+  if (val2 == 2) {
+     val2x = 14;
+     val3 = val3-1
+  }
+  var val4 = parseInt(((val2x+1)*3)/5, 10)
+  var val5 = parseInt(val3/4, 10)
+  var val6 = parseInt(val3/100, 10)
+  var val7 = parseInt(val3/400, 10)
+  var val8 = val1+(val2x*2)+val4+val3+val5-val6+val7+2
+  var val9 = parseInt(val8/7, 10)
+  var val0 = val8-(val9*7)
+  form.result1.value = months[val2]+" "+form.day.value +", "+form.year.value
+  form.result2.value = femaleakannames [val0]
+}   
